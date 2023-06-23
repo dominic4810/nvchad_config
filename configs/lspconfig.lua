ca6bab75eb2ceaf8eee58737e19b72f1607e0cba
Dominic Wong <dominicwong999@gmail.com>
2023-06-23 16:05:32 +0200

    Disabled snippets

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,6 +1,9 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+-- disable snippets
+capabilities.textDocument.completion.completionItem.snippetSupport = false
+
 local lspconfig = require "lspconfig"
 
 -- python lsp
