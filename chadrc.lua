db12135f14684292855d806ebc40fd4c04354c9c
Dominic Wong <dominicwong999@gmail.com>
2023-07-04 16:42:12 +0200

    Reenable netrw

--- chadrc.lua
+++ chadrc.lua
@@ -3,4 +3,5 @@
  M.ui = {theme = 'doomchad'}
  M.plugins = "custom.plugins"
  M.mappings = require "custom.mappings"
+ M.lazy_nvim = require "custom.configs.lazy_nvim" -- config for lazy.nvim startup options
  return M
--- configs/nvimtree.lua
+++ configs/nvimtree.lua
@@ -4,5 +4,7 @@ options.git = {
   enable = true
 }
 options.renderer.icons.show.git = true
+options.disable_netrw = false
+options.hijack_netrw = false
 
 return options
